215e050a9ad639a88692d263c4811294dfab2bac
masha-b <mashabudryte@gmail.com>
2019-04-01 12:19:51 +0100

    bike limit established

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,17 @@
 require_relative 'bike'
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    raise 'No bikes available!' unless @bike
-    @bike
+    raise 'No bikes available!' if @bikes.length == 0
+    @bikes
   end
 
   def dock_bike(bike)
-    raise 'No space available!' if @bike
-    @bike = bike
+    raise 'No space available!' unless @bikes.length < 20
+    @bikes << bike
   end
 end
--- spec/units/docking_station_spec.rb
+++ spec/units/docking_station_spec.rb
@@ -11,10 +11,9 @@ describe DockingStation do
   describe '#dock_bike' do
     it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
-    it 'raises an error when bike is docked alreade' do
-      bike = Bike.new
-      subject.dock_bike(bike)
-      expect { subject.dock_bike(bike) }.to raise_error('No space available!')
+    it 'raises an error when 20 bikes are docked already' do
+      20.times { subject.dock_bike Bike.new }
+      expect { subject.dock_bike(Bike.new) }.to raise_error('No space available!')
     end
   end
 end
